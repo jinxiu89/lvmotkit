66d52517696f4e70f0189a31dd74f916e39698fa
jinxiu89 <jinxiu89@163.com>
2019-05-19 09:06:12 +0800

    jiancang

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,6 @@
  * @author jinxiu89@163.com
  * @copyright 2019
  */
+function route_class(){
+    return str_replace('.','-',Route::currentRouteName());
+}
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,17 @@
 */
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::prefix('admin')->group(function (){
+Route::get('/', 'PagesController@root')->name('root');
+Route::prefix('admin')->group(function () {
     Route::get('/hello/{name}', 'admin\IndexController@index')->where(['name' => '[a-zA-Z]+',]);
 });
+Auth::routes(['verify' => true]);
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware'=>['auth','verified']],function(){
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+});
+
