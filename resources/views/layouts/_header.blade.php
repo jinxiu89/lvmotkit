708cf325b9b0d9c1b9e0059a163d7254f3fa7c03
jinxiu89 <jinxiu89@163.com>
2019-06-02 10:40:15 +0800

    user jiemian products liebiao

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -15,12 +15,14 @@
             <ul class="navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
-                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">login</a></li>
                 @else
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                                 class="img-responsive img-circle" width="30px" height="30px">
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,18 @@
 */
 
 use Illuminate\Support\Facades\Route;
+
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', 'PagesController@root')->name('root');
-Auth::routes(['verify' => true]);
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Auth::routes();
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
-Route::group(['middleware'=>['auth','verified']],function(){
+Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-    Route::get('user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 });
