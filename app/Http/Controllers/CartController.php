aeee2f78385de9accd15edea7d4d40a35bd56d45
jinxiu89 <jinxiu89@163.com>
2019-08-26 11:25:53 +0800

    服务逻辑层

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,39 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\ProductSku;
+use App\Services\CartService;
 use Illuminate\Http\Request;
-use App\Models\CartItem;
 
 class CartController extends Controller
 {
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
+    public function index(Request $request)
+    {
+        $cartItems = $this->cartService->get();
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
+        return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
+    }
+
     public function add(AddCartRequest $request)
     {
-        $user = $request->user();
-        $skuId = $request->input('sku_id');
-        $amount = $request->input('amount');
-        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
         return [];
     }
 
     public function remove(ProductSku $sku, Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
         return [];
     }
 
-    public function index(Request $request)
-    {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
-        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
-        return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
-    }
 }
--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -11,16 +11,21 @@ use App\Models\Order;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Jobs\CloseOrder;
+use App\Services\OrderService;
+
 
 class OrdersController extends Controller
 {
+    protected $OrderService;
+
+    public function __construct(OrderService $orderService)
+    {
+        $this->OrderService = $orderService;
+    }
+
     public function index(Request $requests)
     {
-        $orders = Order::query()
-            ->with(['items.product', 'items.productSku'])
-            ->where('user_id', $requests->user()->id)
-            ->orderBy('created_at', 'desc')
-            ->paginate();
+        $orders = $this->OrderService->all($requests->user()->id);
         return view('orders.index', ['orders' => $orders]);
     }
 
@@ -29,60 +34,15 @@ class OrdersController extends Controller
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
 
+    /***
+     * @param OrderRequest $request
+     * @return mixed
+     * 要有服务的概念,因为你写的程序可能会被应用于其他的方面,例如小程序,app,单页网站等
+     */
     public function store(OrderRequest $request)
     {
         $user = $request->user();
-        // 开启一个数据库事务
-        $order = DB::transaction(function () use ($user, $request) {
-            $address = UserAddress::find($request->input('address_id'));
-            // 更新此地址的最后使用时间
-            $address->update(['last_used_at' => Carbon::now()]);
-            // 创建一个订单
-            $order = new Order([
-                'address' => [ // 将地址信息放入订单中
-                    'address' => $address->full_address,
-                    'zip' => $address->zip,
-                    'contact_name' => $address->contact_name,
-                    'contact_phone' => $address->contact_phone,
-                ],
-                'remark' => $request->input('remark'),
-                'total_amount' => 0,
-            ]);
-            // 订单关联到当前用户
-            $order->user()->associate($user);
-            // 写入数据库
-            $order->save();
-
-            $totalAmount = 0;
-            $items = $request->input('items');
-            // 遍历用户提交的 SKU
-            foreach ($items as $data) {
-                $sku = ProductSku::find($data['sku_id']);
-                // 创建一个 OrderItem 并直接与当前订单关联
-                $item = $order->items()->make([
-                    'amount' => $data['amount'],
-                    'price' => $sku->price,
-                ]);
-                $item->product()->associate($sku->product_id);
-                $item->productSku()->associate($sku);
-                $item->save();
-                $totalAmount += $sku->price * $data['amount'];
-                if ($sku->decreaseStock($data['amount']) <= 0) {
-                    throw new InvalidRequestException('该商品库存不足');
-                }
-            }
-
-            // 更新订单总金额
-            $order->update(['total_amount' => $totalAmount]);
-
-            // 将下单的商品从购物车中移除
-            $skuIds = collect($items)->pluck('sku_id');
-            $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
-            $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-            return $order;
-        });
-
-        return $order;
+        $address=UserAddress::find($request->input('address_id'));
+        return $this->OrderService->store($user,$address,$request->input('remark'),$request->input('items'));
     }
-
 }
